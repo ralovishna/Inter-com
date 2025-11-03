0b8f84dc5c067ac2a3474ce38a9f519213f84346
ralovishna <krishnamalavia.dev@gmail.com>
2025-11-03 14:03:13 +0530

    Feigh done

---
+++ consumer/src/main/java/com/intercomserv/consumer/feign/FeignController.java
@@ -0,0 +1,18 @@
+package com.intercomserv.consumer.feign;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/feign-template")
+@RequiredArgsConstructor
+public class FeignController {
+    private final ProviderFeignClient providerFeignClient;
+
+    @GetMapping("/instance")
+    public String getInstance() {
+        return providerFeignClient.getInstanceInfo();
+    }
+}
